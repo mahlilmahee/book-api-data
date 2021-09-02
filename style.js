0fe5bca0a3990a36b46baa5c1ee8826bbe9cac1a
acc username <acc email>
2021-09-02 16:39:25 +0600

    I have shown undefined

--- style.js
+++ style.js
@@ -1,10 +1,6 @@
 const bookbtn=()=>{
-    // console.log('hello I am here ');
     const inputText=document.getElementById("inputtext");
     const inputInner =inputText.value;
-    // console.log(inputInner);
-    // refreshing the page 
-    
     inputText.value='';
     const bookUrl =`https://openlibrary.org/search.json?q=${inputInner}`;
     fetch(bookUrl)
@@ -23,16 +19,30 @@ const bookName = (data)=>
     // console.log(detailBook);
     // console.log(detailBook);
     detailBook.forEach(book => 
-        {// books name 
-            const bookRealTitle = book.text[1];
-            const booksAuthor=book.author_name[0];
-            const publisherOfTheBook =book.publisher;
-            const publishYear =book.publish_year[0];
+
+        {// books name
+             console.log(book);
+            const bookRealTitle = book.text?.[1];
+            console.log(bookRealTitle);
+            const booksAuthor=book.author_name?.[0];
+            console.log(booksAuthor);
+            const publisherOfTheBook =book.publisher?.[0];
+            console.log(publisherOfTheBook);
+            let publishYear =book.publish_year?.[0];
+            // const variable =book=>
+            // {
+            //    if(book.publish_year[0] ==undefined){
+            //        return booksAuthor;
+            //    }
+            //    else{
+            //        return publishYear;
+            //    }
+            // }
+            // ;
+            // const booksatuo=variable(book);
+           
+            console.log(publishYear);
             let coverId =book.cover_i;
-            if(coverId===undefined){
-                converId=" there is no image sorry "
-            }
-            
              const div =document.createElement('div');
              div.classList.add('col-4');
              div.innerHTML=`
@@ -40,21 +50,12 @@ const bookName = (data)=>
               <p>Books-name: ${bookRealTitle}</p>
               <p>Authors-name :${booksAuthor}</p>
               <p>Publisher-name: ${publisherOfTheBook}</p>
-              <p>Publish date :${publishYear} </p>
-             `;
+              <p>Publish date :${publishYear} </p> `;
              divforBooks.appendChild(div);
-            //  divforBooks.innerHTML='';
+            
             
         })
 }
-
-
-
-
-
-
-
-
 
 // here is a optional code unrelated with the assignment only to add the enter btn connection with the btn so please ignore it 
 document.getElementById("inputtext").addEventListener('keyup',function(event){
