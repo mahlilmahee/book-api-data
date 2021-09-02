1c3f7b565c72cbc13e32ab3737ec5fc5049a5b4f
acc username <acc email>
2021-09-02 18:00:05 +0600

    df

--- style.js
+++ style.js
@@ -6,6 +6,7 @@ const bookbtn=()=>{
     fetch(bookUrl)
     .then(res=>res.json())
     .then(data=>bookName(data));
+   
 }
 const bookName = (data)=>
 {
@@ -15,7 +16,8 @@ const bookName = (data)=>
     totalBook.innerText=books;
     const detailBook=data.docs;
     const divforBooks=document.getElementById('divOnlyOne');
-     divforBooks.textContent='';
+     divforBooks.textContent=''; 
+     checkingValid();
     // console.log(detailBook);
     // console.log(detailBook);
     detailBook.forEach(book => 
@@ -23,23 +25,16 @@ const bookName = (data)=>
         {// books name
              console.log(book);
             const bookRealTitle = book.text?.[1];
-            console.log(bookRealTitle);
+          const bookNumber =checkingUndefined(bookRealTitle); 
             const booksAuthor=book.author_name?.[0];
             console.log(booksAuthor);
+            const authorAuthinfication=checkingUndefined(booksAuthor);
             const publisherOfTheBook =book.publisher?.[0];
             console.log(publisherOfTheBook);
+            const checkingPublisher =checkingUndefined(publisherOfTheBook);
             let publishYear =book.publish_year?.[0];
-            // const variable =book=>
-            // {
-            //    if(book.publish_year[0] ==undefined){
-            //        return booksAuthor;
-            //    }
-            //    else{
-            //        return publishYear;
-            //    }
-            // }
-            // ;
-            // const booksatuo=variable(book);
+        
+        const ccumma =checkingUndefined(publishYear);
            
             console.log(publishYear);
             let coverId =book.cover_i;
@@ -47,20 +42,41 @@ const bookName = (data)=>
              div.classList.add('col-4');
              div.innerHTML=`
               <img width:"100%" class="img-fluid" src=" https://covers.openlibrary.org/b/id/${coverId}-M.jpg" alt="">
-              <p>Books-name: ${bookRealTitle}</p>
-              <p>Authors-name :${booksAuthor}</p>
-              <p>Publisher-name: ${publisherOfTheBook}</p>
-              <p>Publish date :${publishYear} </p> `;
+              <p>Books-name: ${bookNumber}</p>
+              <p>Authors-name :${authorAuthinfication}</p>
+              <p>Publisher-name: ${checkingPublisher}</p>
+              <p>Publish date :${ccumma} </p> `;
              divforBooks.appendChild(div);
             
             
         })
 }
-
+// function for checking whether it is undefined or not 
+const checkingUndefined=info =>{
+    if( info==undefined){
+        info=" Right now it is not available ";
+        return info;
+    }
+    else {
+        return info;
+    } 
+}
 // here is a optional code unrelated with the assignment only to add the enter btn connection with the btn so please ignore it 
 document.getElementById("inputtext").addEventListener('keyup',function(event){
     if(event.keyCode===13){
         event.preventDefault();
         document.getElementById('Btn').click();
     }
-})
+});
+const checkingValid= ()=>{
+     const valueCheckign =document.getElementById("bookNumber").innerText;
+     const valueToInt =parseInt(valueCheckign);
+     console.log(valueToInt);
+     if(valueCheckign=="0"){
+        
+          const divforBooks=document.getElementById('divOnlyOne');
+          divforBooks.textContent='Your input is not valid please check it again and write it again';
+        
+
+     }
+}
