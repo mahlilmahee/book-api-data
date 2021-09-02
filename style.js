b4cbbbc69b90dd17f2fe1e67dc0efce7936c0df4
acc username <acc email>
2021-09-02 13:27:27 +0600

    hello

--- style.js
+++ style.js
@@ -3,43 +3,47 @@ const bookbtn=()=>{
     const inputText=document.getElementById("inputtext");
     const inputInner =inputText.value;
     // console.log(inputInner);
+    // refreshing the page 
+    
     inputText.value='';
     const bookUrl =`https://openlibrary.org/search.json?q=${inputInner}`;
     fetch(bookUrl)
     .then(res=>res.json())
-    .then(data=>bookName(data))
+    .then(data=>bookName(data));
 }
 const bookName = (data)=>
 {
-    console.log(data);
+    // console.log(data);
     const totalBook = document.getElementById('bookNumber');
     const books =data.numFound;
     totalBook.innerText=books;
     const detailBook=data.docs;
+    const divforBooks=document.getElementById('divOnlyOne');
+     divforBooks.textContent='';
+    // console.log(detailBook);
     // console.log(detailBook);
     detailBook.forEach(book => 
-        {
-            console.log(book);
-            // books name 
+        {// books name 
             const bookRealTitle = book.text[1];
-            console.log(bookRealTitle);
-            // authors name 
             const booksAuthor=book.author_name[0];
             const publisherOfTheBook =book.publisher;
             const publishYear =book.publish_year[0];
-            const divforBooks=document.getElementById('divOnlyOne');
+            let coverId =book.cover_i;
+            if(coverId===undefined){
+                converId=" there is no image sorry "
+            }
+            
              const div =document.createElement('div');
              div.classList.add('col-4');
              div.innerHTML=`
-             
-             <img src="" alt="">
-             <p>Books-name: ${bookRealTitle}</p>
-             <p>Authors-name :${booksAuthor}</p>
-             <p>Publisher-name: ${publisherOfTheBook}</p>
-             <p>Publish date :${publishYear} </p>
-         
+              <img width:"100%" class="img-fluid" src=" https://covers.openlibrary.org/b/id/${coverId}-M.jpg" alt="">
+              <p>Books-name: ${bookRealTitle}</p>
+              <p>Authors-name :${booksAuthor}</p>
+              <p>Publisher-name: ${publisherOfTheBook}</p>
+              <p>Publish date :${publishYear} </p>
              `;
              divforBooks.appendChild(div);
+            //  divforBooks.innerHTML='';
             
         })
 }
